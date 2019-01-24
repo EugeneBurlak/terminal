59726494d8257ec0f07d2dbee4976accb266d915
Eugene <eugeneburlak@yandex.ru>
2019-01-24 23:11:28 +0300

    upd

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import { routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
 import reducers from '../reducers';
 
-export const history = createHistory();
+export const history = createHistory({ basename: process.env.PUBLIC_URL });
 const middleware = routerMiddleware(history);
 export const store =    createStore(
                             reducers,
