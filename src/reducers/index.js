e80945b833e9ba700c7ca41108ce5db12d2d4a93
e.burlak <e.burlak@sobix.net>
2019-01-25 14:16:11 +0300

    clear code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import appReducer from './appReducer';
 import terminalReducer from './terminalReducer';
 import { routerReducer } from 'react-router-redux';
 
 export default combineReducers({
 	routing: routerReducer,
-	appReducer,
 	terminalReducer
 });
